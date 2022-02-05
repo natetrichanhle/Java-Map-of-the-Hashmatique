00cf28ff0cf186c85bf0ea4c2d87dca1929e285d
Nate Trichanh <natetrichanh@gmail.com>
2022-02-05 14:17:56 -0800

    initial commit

---
+++ Java Foundations/Fundamentals/First-Java-Program/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Nate Trichanh");
+        System.out.println("I am 19 years old");
+        System.out.println("My hometown is Orange County, CA");
+    }
+}
